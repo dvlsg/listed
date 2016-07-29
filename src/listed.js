ccb1b75f641d757f03babbe109677355cde3b52a
Dave Lesage <dlesage27@gmail.com>
2016-07-28 19:54:19 -0700

    feat: add List.from()

--- src/listed.js
+++ src/listed.js
@@ -4,6 +4,16 @@ class List extends Array {
   get [Symbol.toStringTag]() {
     return 'List';
   }
+
+  static from(arraylike) {
+    const length = arraylike.length >>> 0;
+    const list = new List(length);
+    let index = -1;
+    while (++index < length) {
+      list[index] = arraylike[index];
+    }
+    return list;
+  }
 }
 
 module.exports = {
