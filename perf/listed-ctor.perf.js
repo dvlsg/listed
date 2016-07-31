b9befdb7d0b6e323899de0de34b39ecb07fcac85
Dave Lesage <dlesage27@gmail.com>
2016-07-31 13:59:02 -0700

    chore: ctor bench cleanup, add List.from(iterable)

--- perf/listed-ctor.perf.js
+++ perf/listed-ctor.perf.js
@@ -4,42 +4,25 @@ const run = require('./util/run');
 const fill = require('./util/fill');
 const { List } = require('../');
 
-let args = fill(0, 500);
+const args = fill(0, 500);
 
-function listConstructor() {
-  return new List(...args);
-}
-
-function arrayConstructor() {
-  return new Array(...args);
-}
-
-function arrayLiteral() {
-  return [ ...args ];
-}
+const listConstructor = () => new List(...args);
+const arrayConstructor = () => new Array(...args);
+const arrayLiteral = () => [ ...args ];
 
 // the spread is redundant with .from(),
 // but lets keep one spread per method to try
 // to have the methods as similar as possible.
-function listFrom() {
-  return List.from([ ...args ]);
-}
-
-function arrayFrom() {
-  return Array.from([ ...args ]);
-}
-
-function listOf() {
-  return List.of(...args);
-}
-
-function arrayOf() {
-  return Array.of(...args);
-}
+const listFromArraylike = () => List.from([ ...args ]);
+const listFromIterable = () => List.from([ ...args ][Symbol.iterator]());
+const arrayFrom = () => Array.from([ ...args ]);
+const listOf = () => List.of(...args);
+const arrayOf = () => Array.of(...args);
 
 run('constructors', {
+  'List.from(arraylike)': listFromArraylike,
+  'List.from(iterable)': listFromIterable,
   'List.of()': listOf,
-  'List.from()': listFrom,
   'List constructor': listConstructor,
   'Array constructor': arrayConstructor,
   'Array literal': arrayLiteral,
