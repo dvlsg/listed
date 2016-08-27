ffa20cd4b55bd5a955a1bb706ef7158fdac93783
Dave Lesage <dlesage27@gmail.com>
2016-08-27 11:27:18 -0700

    test: add List#last() coverage

--- test/listed.spec.js
+++ test/listed.spec.js
@@ -224,6 +224,29 @@ describe('List', () => {
     });
   });
 
+  describe('#last()', () => {
+    it('should return the last element', () => {
+      let list = List.of(1, 2, 3);
+      let actual = list.last();
+      let expected = 3;
+      assert.strictEqual(actual, expected);
+    });
+
+    it('should work with single item lists', () => {
+      let list = List.of(1);
+      let actual = list.last();
+      let expected = 1;
+      assert.strictEqual(actual, expected);
+    });
+
+    it('should return undefined from empty list', () => {
+      let list = new List();
+      let actual = list.last();
+      let expected = undefined; // eslint-disable-line no-undef-init
+      assert.strictEqual(actual, expected);
+    });
+  });
+
   describe('#flatten()', () => {
     it('should return a new List', () => {
       let list = new List();
